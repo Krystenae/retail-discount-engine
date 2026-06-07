cc1b226ceba2bf4ed675a1f9b6c61beaac818cf8
Krystenae Dennis <krystenaedennis@gmail.com>
2026-06-07 19:15:27 -0400

    created product inventory and HTML file

---
+++ cc_4.js
@@ -0,0 +1,7 @@
+let products = [
+    { name: "Laptop", category: "electronics", price: 1000, inventory: 10 },
+    { name: "T-Shirt", category: "apparel", price: 30, inventory: 20 },
+    { name: "Milk", category: "groceries", price: 5, inventory: 50 },
+    { name: "Detergent", category: "household", price: 12, inventory: 25 },
+    { name: "Book", category: "other", price: 20, inventory: 15 }
+];
